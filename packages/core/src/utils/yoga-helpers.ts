da96422734ce3d13dab4140d693eea5dad55ffcd
Law <lkuoch@users.noreply.github.com>
2024-08-25 00:55:44 +1000

    sauce

--- packages/core/src/utils/yoga-helpers.ts
+++ packages/core/src/utils/yoga-helpers.ts
@@ -1,16 +1,17 @@
 import { blockFieldSuggestionsPlugin } from '@escape.tech/graphql-armor-block-field-suggestions'
 import { useDeferStream } from '@graphql-yoga/plugin-defer-stream'
 import { useDisableIntrospection } from '@graphql-yoga/plugin-disable-introspection'
-import { GraphQLParams, Plugin, YogaInitialContext } from 'graphql-yoga'
-import { createStellateLoggerPlugin } from 'stellate/graphql-yoga'
+import type { GraphQLParams, Plugin, YogaInitialContext } from 'graphql-yoga'
+import type { ServerResponse } from 'node:http'
 
 export type InitialContext = {
   headers: Headers
   params: GraphQLParams
   request: YogaInitialContext['request']
+  response: ServerResponse
 }
 
-export type UserContext = Record<string, any>
+export type UserContext = any
 
 export type GetContext<AdditionalContext> =
   | ((ctx: InitialContext) => UserContext & AdditionalContext)
@@ -27,12 +28,6 @@ export const getYogaPlugins = (stellate?: StellateOptions): Plugin[] => {
     useDeferStream(),
     process.env.NODE_ENV === 'production' && useDisableIntrospection(),
     process.env.NODE_ENV === 'production' && blockFieldSuggestionsPlugin(),
-    Boolean(process.env.NODE_ENV === 'production' && stellate) &&
-      createStellateLoggerPlugin({
-        serviceName: stellate!.serviceName,
-        token: stellate!.loggingToken,
-        fetch,
-      }),
   ].filter(Boolean) as Plugin[]
 }
 
@@ -42,6 +37,7 @@ export const wrappedContext = <AdditionalContext extends Record<string, any>>(
   return async (ct) => {
     const baseContext: InitialContext = {
       request: ct.request,
+      response: ct.res,
       headers: ct.request.headers,
       params: ct.params,
     }
@@ -58,7 +54,9 @@ export const wrappedContext = <AdditionalContext extends Record<string, any>>(
         ...baseContext,
         ...userCtx,
       }
-    } else if (typeof context === 'object') {
+    }
+
+    if (typeof context === 'object') {
       return {
         ...baseContext,
         ...context,
